2a8f42d88f62125431e7ae51cef6df0bb442ef5f
Vashisth-2604 <158995142+Vashisth-2604@users.noreply.github.com>
2024-02-05 10:53:01 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail ="Abhishek@google.com"
+var accountPassword ="12345"
+accountCity = "jaipur"
+let accountState;
+// accountId =2 // 
+
+console.log(accountId)
+
+accountEmail= "hc@hc.com"
+accountPassword = "1212121"
+accountCity = "Bengaluru"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use Strict"  //  treat all JS code as newer version
+
+//alert("hello")  // we are using node js,not browser
+
+console.log(3+
+    3)  // code readability should br high
+
+console.log("Abhishek")  
+
+let name = "abhishek"
+let age = 25
+let loggedIn = true 
+
+
+
+
+// number ==> 2 the pwr of 53
+//bigInt
+//string ==> ""
+// boolean ==> true/false
+//null ==> standalone value
+//undefined ==> 
+//symbol ==>
+
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
