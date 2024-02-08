5bb81c573046cc2f8b244362e3642fc2cd911e97
Vashisth-2604 <158995142+Vashisth-2604@users.noreply.github.com>
2024-02-08 19:28:55 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// primitive
+
+// 7 Types: String,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+const score=100
+const scorevalue=100.3
+
+const isloggedIn = false
+const outsidetemp = null;
+let userEmail;
+
+const Id =  Symbol('123')
+const anotherId = Symbol('123')
+console.log(Id === anotherId)
+
+//const bigNumber= 3564478838378383n
+
+
+// Reference Type(Non-primitive)
+
+//Array,Objects,Functions
+
+const heros =["Shaktiman","Naagraj","doga"];
+
+let myobj ={
+    name:"Abhishek",
+    age: 22, 
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof outsideTemp)
