5e5144c2c14484edde0a9a4a6c25bebb33851f1e
Vashisth-2604 <158995142+Vashisth-2604@users.noreply.github.com>
2024-02-27 05:35:25 +0000

    for Each

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,1 +1,33 @@
 const coding =["js","ruby","java","python","cpp"]
+//coding.forEach(function (val){
+  //    console.log(val);
+//})
+
+//coding.forEach((item) => {
+  //  console.log(item);
+//} )
+  // function printMe(item) {
+   // console.log(item);
+//} 
+
+//coding.forEach(printMe)
+
+ 
+
+const myCoding = [
+    {
+        languageName:"javascript",
+        languageFilename:"js"
+    },
+    {
+        languageName:"java",
+        languageFilename:"java"
+    },
+    {
+        languageName:"Python",
+        languageFilename:"py"
+    },
+]
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
