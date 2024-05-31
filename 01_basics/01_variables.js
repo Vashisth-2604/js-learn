8b989595133477a1049f2962c8f0570d1c9f9f11
Vashisth-2604 <158995142+Vashisth-2604@users.noreply.github.com>
2024-05-31 08:04:29 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -11,5 +11,9 @@ accountEmail= "hc@hc.com"
 accountPassword = "1212121"
 accountCity = "Bengaluru"
 
-console.log(accountId)
+console.log(accountId);
+
+/* prefer not to use var
+because of issue in block scope and functional scope
+*/
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
